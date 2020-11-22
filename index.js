ede74fe31a9dc8f7678f7dbcc66addf051784d3a
milanwinter <milanwinter8@gmail.com>
2020-11-22 12:40:45 -0800

    commit tree 25d576585e5d81164329ab773f7d98051eea61d5

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    }
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+      
+      
+      fetch("http://localhost:3000/users", configObj)
+      .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+        let body = document.querySelector("body")
+          const id = object.id
+            body.innerHTML += id
+      })
+      .catch(function(error) {
+        let body = document.querySelector("body")
+        const message = error.message
+        body.innerHTML += message
+      })
+}
